320c9ba7bd53fcba6769e2bbb4b9710b508e4ac3
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2025-07-31 16:24:13 +0100

    Updated filament admin for adding feature item to any model

--- src/Models/FeatureItem.php
+++ src/Models/FeatureItem.php
@@ -3,17 +3,23 @@
 namespace SaasPro\Features\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use SaasPro\Enums\Timelines;
 
 class FeatureItem extends Model {
     
     protected $fillable = ['feature_id', 'limit', 'reset_period', 'reset_interval' ];
 
-    function featurable(){
+    protected $casts = [
+        'limit' => 'integer',
+        'reset_period' => 'integer',
+        'reset_interval' => Timelines::class,
+    ];
+
+    function featureable(){
         return $this->morphTo();
     }
-
     function feature(){
-        return $this->belongsTo(Feature::class, 'feature_id');
+        return $this->belongsTo(Feature::class);
     }
 
 
